622f773dac001a43de5d0c0cdcde8f285f33bb6d
Mamadou <tafarus3@gmail.com>
2026-02-16 00:48:00 -0500

    SCRUM-1 setup render

--- sylistock/sylistock/settings.py
+++ sylistock/sylistock/settings.py
@@ -21,7 +21,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -38,8 +37,6 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 # Parse ALLOWED_HOSTS from environment or use default
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-
-
 
 # Application definition
 
@@ -87,7 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'sylistock.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
