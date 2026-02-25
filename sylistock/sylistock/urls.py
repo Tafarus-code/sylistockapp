1514b80a2bf5be41217c29cebfe869f24734c877
Mamadou <tafarus3@gmail.com>
2026-02-25 17:22:27 -0500

    SCRUM-1 added setup for flutter UI and connection to django backend

--- sylistock/sylistock/urls.py
+++ sylistock/sylistock/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from sylistockapp.views_home import api_home
 
 urlpatterns = [
+    path('', api_home, name='api-home'),
     path('admin/', admin.site.urls),
 
     # Versioning your API is a Fintech "Must-Have"
