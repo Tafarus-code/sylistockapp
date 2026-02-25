bbc96eade74fa6b34637ea0dca71d62e3b4d20e1
Mamadou <tafarus3@gmail.com>
2026-02-25 17:31:03 -0500

    SCRUM-1 added setup for flutter UI and connection to django backend

--- sylistock/sylistock/urls.py
+++ sylistock/sylistock/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from sylistockapp.views_home import api_home
+from .views_home import api_home
 
 urlpatterns = [
     path('', api_home, name='api-home'),
